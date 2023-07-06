2b04a3b13aba69c3095c9d90b9dde0d71399194b
Crowdbotics <engineering@crowdbotics.com>
2023-07-06 18:36:42 +0000

    Update store.

--- store/daveotestAPI/api.js
+++ store/daveotestAPI/api.js
@@ -7,10 +7,10 @@ function api_v1_login_create(payload) {
   return daveotestAPI.post(`/api/v1/login/`)
 }
 function api_v1_signup_create(payload) {
-  return daveotestAPI.post(`/api/v1/signup/`, payload.data)
+  return daveotestAPI.post(`/api/v1/signup/`, payload)
 }
 function rest_auth_login_create(payload) {
-  return daveotestAPI.post(`/rest-auth/login/`, payload.data)
+  return daveotestAPI.post(`/rest-auth/login/`, payload)
 }
 function rest_auth_logout_list(payload) {
   return daveotestAPI.get(`/rest-auth/logout/`)
@@ -19,31 +19,28 @@ function rest_auth_logout_create(payload) {
   return daveotestAPI.post(`/rest-auth/logout/`)
 }
 function rest_auth_password_change_create(payload) {
-  return daveotestAPI.post(`/rest-auth/password/change/`, payload.data)
+  return daveotestAPI.post(`/rest-auth/password/change/`, payload)
 }
 function rest_auth_password_reset_create(payload) {
-  return daveotestAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return daveotestAPI.post(`/rest-auth/password/reset/`, payload)
 }
 function rest_auth_password_reset_confirm_create(payload) {
-  return daveotestAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return daveotestAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
 function rest_auth_registration_create(payload) {
-  return daveotestAPI.post(`/rest-auth/registration/`, payload.data)
+  return daveotestAPI.post(`/rest-auth/registration/`, payload)
 }
 function rest_auth_registration_verify_email_create(payload) {
-  return daveotestAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return daveotestAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
 function rest_auth_user_read(payload) {
   return daveotestAPI.get(`/rest-auth/user/`)
 }
 function rest_auth_user_update(payload) {
-  return daveotestAPI.put(`/rest-auth/user/`, payload.data)
+  return daveotestAPI.put(`/rest-auth/user/`, payload)
 }
 function rest_auth_user_partial_update(payload) {
-  return daveotestAPI.patch(`/rest-auth/user/`, payload.data)
+  return daveotestAPI.patch(`/rest-auth/user/`, payload)
 }
 export const apiService = {
   api_v1_login_create,
